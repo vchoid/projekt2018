3523685678ee37bdd17e219058f7f42f5967fce6
Christoph Kiank <christoph.kiank@bitmarck.de>
2018-01-16 16:14:34 +0100

    unsinnigen Code gelöscht
    Change-Id: I0a855937b0204558b300104922e9aa552a68649f

--- src/main/java/view/ViewController.java
+++ src/main/java/view/ViewController.java
@@ -5,16 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import javafx.application.Application;
-import javafx.beans.property.ReadOnlyIntegerWrapper;
-import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
 
 public class ViewController implements Initializable{
 
@@ -25,10 +19,7 @@ public class ViewController implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		TableColumn id = new TableColumn("ID");
-		id.setCellValueFactory(cellData -> {
-            Integer rowIndex = cellData;
-            return new ReadOnlyIntegerWrapper(intValues.get(rowIndex));
-        });;
+		
 		TableColumn name = new TableColumn("Name");
 		TableColumn age = new TableColumn("Age");
 		TableColumn email = new TableColumn("Email");
