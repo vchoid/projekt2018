b3ee337abee31aa226d6a98dcbc67d26199279a1
Christoph Kiank <christoph.kiank@bitmarck.de>
2018-01-16 15:54:39 +0100

    Werte in Spalten schreiben
    Change-Id: Id112760f7746195b55660fb52726faea6c1c092e

--- src/main/java/view/ViewController.java
+++ src/main/java/view/ViewController.java
@@ -1,23 +1,44 @@
 package main.java.view;
 
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.application.Application;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import main.java.utils.TableHandler;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class ViewController implements Initializable{
 
 	@FXML private TableView tab1;
 	
+	 List intValues = Arrays.asList(1, 2, 3, 4, 5);
+	 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		TableColumn id = new TableColumn("ID");
+		id.setCellValueFactory(cellData -> {
+            Integer rowIndex = cellData;
+            return new ReadOnlyIntegerWrapper(intValues.get(rowIndex));
+        });;
+		TableColumn name = new TableColumn("Name");
+		TableColumn age = new TableColumn("Age");
+		TableColumn email = new TableColumn("Email");
 		
-		TableHandler port1 = new TableHandler(tab1, "SE");
-		TableHandler port1 = new TableHandler(tab1, "SE");
+		tab1.getColumns().addAll(id,name,age,email);
 	}
+
+	
+
+	
 	
 	
 	
